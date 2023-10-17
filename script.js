6f80257b50554f872ee89127b6f26f54a3899136
Mdimam0786 <142109698+Mdimam0786@users.noreply.github.com>
2023-10-17 23:44:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ function rot13(encodedStr) {
 	const string = encodedStr;
 	const arr = [];
 	for(let i =0; i<string.length; i++){
-		String.charAt[i] = string.charAt[i+13];
+		string.charAt[i] = string.charAt[i+13];
 		arr.push(string[i]);
 	}
 
